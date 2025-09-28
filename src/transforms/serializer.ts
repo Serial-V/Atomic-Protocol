c907665eaf599dbfc5e0d6aa00f53de77dbd0754
Divinfall <divinfall@proton.me>
2025-09-27 20:52:17 -0400

    More Improvements

--- src/transforms/serializer.ts
+++ src/transforms/serializer.ts
@@ -2,35 +2,34 @@ import { Compiler, FullPacketParser, Serializer } from "protodef";
 import protocol from "../config/protocol.json";
 import { config } from "../config/config";
 
-class BufferPool {
-    private pool: Buffer[] = [];
-    constructor(private size: number = 65536) { }
+class RingBufferPool {
+    private buffers: Buffer[];
+    private index = 0;
 
-    alloc(len: number) {
-        if (this.pool.length) return this.pool.pop()!.slice(0, len);
-        return Buffer.allocUnsafe(len);
+    constructor(poolSize = 128, bufferSize = 65536) {
+        this.buffers = Array.from({ length: poolSize }, () => Buffer.allocUnsafe(bufferSize));
     }
 
-    free(buf: Buffer) {
-        this.pool.push(buf);
+    next(len: number) {
+        const buf = this.buffers[this.index];
+        this.index = (this.index + 1) % this.buffers.length;
+        return buf.subarray(0, len);
     }
 }
 
-const bufferPool = new BufferPool(65536);
+const bufferPool = new RingBufferPool(128, 65536);
 
 class Parser extends FullPacketParser {
     verify(deserialized: any, serializer: Serializer) {
         if (!config.debug) return;
-
         const { name, params } = deserialized.data;
         const oldBuffer = deserialized.fullBuffer;
-
         const newBuffer = serializer.createPacketBuffer({ name, params });
-        if (!newBuffer.equals(oldBuffer)) {
-            console.warn("Packet Mismatch", name);
-        }
+
+        if (!newBuffer.equals(oldBuffer)) console.warn("Packet Mismatch", name);
     }
 }
+
 
 class CustomCompiler extends Compiler.ProtoDefCompiler {
     public addTypesToCompilePublic(types: any) {
@@ -67,63 +66,9 @@ export const createDeserializer = () => {
 };
 
 export const serializeWithPool = (serializer: Serializer, packet: any) => {
-    const buf = bufferPool.alloc(65536);
+    const buf = bufferPool.next(65536);
     const result = serializer.createPacketBuffer(packet);
-    bufferPool.free(buf);
-    return result;
+
+    result.copy(buf, 0, 0, result.length);
+    return buf.subarray(0, result.length);
 };
-
-
-
-// import { Compiler, FullPacketParser, Serializer } from "protodef";
-
-// import protocol from "../config/protocol.json";
-// import { config } from "../config/config";
-
-// class Parser extends FullPacketParser {
-//     verify(deserialized: any, serializer: Serializer) {
-//         const { name, params } = deserialized.data;
-
-//         const oldBuffer = deserialized.fullBuffer;
-//         if (config.debug) {
-//             const newBuffer = serializer.createPacketBuffer({ name, params });
-//             if (!newBuffer.equals(oldBuffer)) {
-//                 console.warn('Packet Mismatch', name);
-//             }
-//         }
-//     }
-// }
-
-// class CustomCompiler extends Compiler.ProtoDefCompiler {
-//     public addTypesToCompilePublic(types: any) {
-//         this.addTypesToCompile(types);
-//     }
-// }
-
-// let cachedCompiledProto: any = null;
-// let cachedSerializer: Serializer | null = null;
-// let cachedDeserializer: Parser | null = null;
-
-// const getCompiledProto = () => {
-//     if (!cachedCompiledProto) {
-//         const compiler = new CustomCompiler();
-//         compiler.addTypesToCompilePublic(protocol.types);
-//         compiler.addTypes(require("../datatypes/compiler").default);
-//         cachedCompiledProto = compiler.compileProtoDefSync();
-//     }
-//     return cachedCompiledProto;
-// };
-
-// export const createSerializer = () => {
-//     if (!cachedSerializer) {
-//         cachedSerializer = new Serializer(getCompiledProto(), "mcpe_packet");
-//     }
-//     return cachedSerializer;
-// };
-
-// export const createDeserializer = () => {
-//     if (!cachedDeserializer) {
-//         cachedDeserializer = new Parser(getCompiledProto(), "mcpe_packet");
-//     }
-//     return cachedDeserializer;
-// };
